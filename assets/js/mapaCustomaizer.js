b657b5fa68dfabef6e94872e1893f865105c15b6
Victor Chapa C <victor.260690@gmail.com>
2013-10-30 16:35:56 -0600

    Commented the coordenades debugger.

--- assets/js/mapaCustomaizer.js
+++ assets/js/mapaCustomaizer.js
@@ -14,13 +14,13 @@ function placeMarker(position, mapTable, markerSaver){
 function mapApplication(){
 
     // Bugger for get coordenates of googlemaps and fix bad street view markers.
-    var markerSaver = [];
-    google.maps.event.addListener(mapTable.map, 'click', function(e){
-        if(markerSaver.length == 1){
-            markerSaver[0].setMap(null);
-            markerSaver = new Array();
-        }
-        placeMarker(e.latLng, mapTable, markerSaver);
-    });
+    //var markerSaver = [];
+    //google.maps.event.addListener(mapTable.map, 'click', function(e){
+        //if(markerSaver.length == 1){
+            //markerSaver[0].setMap(null);
+            //markerSaver = new Array();
+        //}
+        //placeMarker(e.latLng, mapTable, markerSaver);
+    //});
     //--------------------------------------------------------------------------
 }
