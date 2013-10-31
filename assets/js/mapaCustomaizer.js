afc220519c0434e7c439b8820fe9b6ac10103154
Victor Chapa C <victor.260690@gmail.com>
2013-10-30 18:50:00 -0600

    Added InfoMarker with spesifications of the propertie.

--- assets/js/mapaCustomaizer.js
+++ assets/js/mapaCustomaizer.js
@@ -29,20 +29,44 @@ function drawingMarkers(data){
     console.log(data);
     var allPropertiesMarkers = new Array();
     var image = new google.maps.MarkerImage("assets/img/house_green.png");
-    _.each(data, function(properties){
 
-        var coordenades = properties.Coordenadas.split(",");
+    _.each(data, function(propertie){
+        var coordenades = propertie.Coordenadas.split(",");
         coordenades[0] = parseFloat(coordenades[0]);
         coordenades[1] = parseFloat(coordenades[1]);
 
         var marker = new google.maps.Marker({
             position: new google.maps.LatLng(coordenades[0], coordenades[1]),
             map: mapTable.map,
-            idPropiedad: properties.IdPropiedad,
+            idPropiedad: propertie.IdPropiedad,
             icon: image
         });
 
         allPropertiesMarkers.push(marker);
+
+        var contentString = '<div class="infoArea">'+
+        '<div class="imgInfo">' +
+        '<img src="api/'+ propertie.Foto1 + '">' +
+        '</div>' +
+        '</div>' +
+        '<div class="infoData">'+
+        '<p>Precio: $'+ propertie.Precio + " (" + propertie.TOperacion + ")</p>"+
+        '</div>'+
+        '<div class="interacition">'+
+        '<a href="ver_propiedad.php?id=' + propertie.IdPropiedad + '">' +
+        '<input class="btn btn-default moreDetails" type="button" value="Más detalles">'+
+        '</a>' +
+        '</div>'+
+        '</div>';
+
+        var markerInfo = new google.maps.InfoWindow({
+            content: contentString,
+        });
+
+        google.maps.event.addListener(marker, "click", function(){
+            markerInfo.open(marker.get('map'), marker);
+        });
+
     });
 
     console.log(allPropertiesMarkers);
