625f18a6329d94faf61cdc49d7b773fc4f85c53c
isracortes62 <isracortes62@gmail.com>
2013-11-04 16:36:54 -0600

    Modified view property, now it's a template

---
+++ templates/property.js
@@ -0,0 +1,35 @@
+TEMPLATES.property = 
+    "<script type='text/template' id='thisTemplate'>" + 
+    "<div class='propertyContainer'>" +
+        "<div class='property'>" +
+            "<h1>Detalles de la propiedad</h1>" +
+            "<input type= 'text' hidden='true' name='id_propiedad' value='<% print(property.IdPropiedad) %>'>" +
+            "<p>Dirección: <% print(property.Direccion) %></p>" +
+            "<p>Colonia: <% print(property.Colonia) %></p>" +
+            "<p>Estado: <% print(property.Estado) %></p>" +
+            "<p>Municipio: <% print(property.Municipio) %></p>" +
+            "<p>Tamaño de la construcción: <% print(property.Construccion) %>"+" "+"<% print(property.Unidad1) %></p>" +
+            "<p>Tamaño del terreno: <% print(property.Terreno)"+" "+"<% print(property.Unidad2) %></p>" +
+            "<p>Fecha de inicio: <% print(property.FInicio) %></p>" +
+            "<p>Fecha de cierre: <% print(property.FFin') %></p>" +
+            "<p>Descripción: <% print(property.Descripcion) %></p>" +
+            "<a href='api/'><img src='api/' width='150' height='150'></a>" +
+            "<% if() %>" +
+            "<% if (property.Foto2 != ""){" +
+            "print('<a href='api/'><img src='api' width='150' height='150'></a>')" +
+            "} %>"+
+            "<% if (property.Foto3 != ""){" +
+            "print('<a href='api/'><img src='api' width='150' height='150'></a>')" +
+            "} %>"+
+            "<% if (property.Foto4 != ""){" +
+            "print('<a href='api/'><img src='api' width='150' height='150'></a>')" +
+            "} %>"+
+            "<% if (property.Foto5 != ""){" +
+            "print('<a href='api/'><img src='api' width='150' height='150'></a>')" +
+            "} %>"+
+            "<% if (property.Foto6 != ""){" +
+            "print('<a href='api/'><img src='api' width='150' height='150'></a>')" +
+            "} %>"+
+        "</div>" +
+    "</div>" +
+    "</script>";
