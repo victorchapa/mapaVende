4657eec8dd7322ed3568755a90448901ce699115
Victor Chapa C <victor.260690@gmail.com>
2013-11-03 20:16:54 -0600

    Added initilizer for the sliders.

--- assets/js/app/views/filtersView.js
+++ assets/js/app/views/filtersView.js
@@ -5,8 +5,8 @@ var FiltersView = Backbone.View.extend({
         "click .f1"         : "getValues",
         "click .f2"         : "getValues",
         "click .f3"         : "getValues",
-        "mouseup .f4"       : "filterBy4",
-        "mouseup .f5"       : "filterBy5",
+        "mouseup .f4"       : "filterByP",
+        "mouseup .f5"       : "filterByH",
     },
     
     getValues: function(e){
@@ -29,6 +29,10 @@ var FiltersView = Backbone.View.extend({
     },
 
     validation: function(valueF1, valueF2, valueF3, valueF4, valueF5, target){
+        var self = this;
+        newRankP = [];
+        newRankH = [];
+        
 
         console.log("-----");
         console.log(valueF1);
@@ -50,6 +54,17 @@ var FiltersView = Backbone.View.extend({
                 if((secondSelectTag == "seleccionar") && (valueF3 == undefined)){
                     console.log("Ninguno extra");
                     console.log(firstSelectTag);
+                    _.each(allPropertiesMarkers, function(marker){
+                        if(marker.tOperacion != undefined){
+                            marker.setVisible(true);
+                            self.setNewRankP(marker.precio, false);
+                            self.setNewRankH(marker.habitaciones, false);
+                        }else{
+                            marker.setVisible(false);
+                        }
+                    });
+                    self.setNewRankP("", true);
+                    self.setNewRankH("", true);
                 }else if((secondSelectTag != "seleccionar") && (valueF3 == undefined)){
                     console.log("Seleccionar tbn F2");
                     console.log(firstSelectTag);
@@ -84,6 +99,18 @@ var FiltersView = Backbone.View.extend({
             }else{
                 console.log("Solo el.");
                 console.log(firstSelectTag);
+                _.each(allPropertiesMarkers, function(marker){
+                    if(marker.tOperacion == firstSelectTag){
+                        marker.setVisible(true);
+                        self.setNewRankP(marker.precio, false);
+                        self.setNewRankH(marker.habitaciones, false);
+                    }else{
+                        marker.setVisible(false);
+                    }
+                    console.log(marker);
+                });
+                self.setNewRankP("", true)
+                self.setNewRankH("", true)
             }
         }
         if(firstSelectTag == undefined){
@@ -99,6 +126,121 @@ var FiltersView = Backbone.View.extend({
             }
         }
      
+        visibleMarkers = this.getVisibleMarkers();
+    },
+    
+    setNewRankP: function(price, completed){
+        if(completed != true){
+            newRankP.push(price); 
+        }else{
+            console.log(newRankP);
+            if(newRankP.length == 1){
+                console.log("Solo uno");
+                this.renderSliderPrices(parseFloat(newRankP[0]), 1);
+            }else if(newRankP.length >= 2){
+                var menor = parseFloat(newRankP[0]);
+                var mayor = 1;
+                _.each(newRankP, function(price){
+                    if(parseFloat(price) >= mayor){
+                        mayor = parseFloat(price);
+                        menor = menor;
+                    }else{
+                        mayor = menor;
+                        menor = parseFloat(price)
+                    }
+                });
+                this.renderSliderPrices(mayor, menor);
+            }else{
+                this.renderSliderPrices(0, 0);
+            }
+        }
+    },
+
+    setNewRankH: function(hab, completed){
+        if(completed != true){
+            newRankH.push(hab); 
+        }else{
+            console.log(newRankP);
+            if(newRankH.length == 1){
+                console.log("Solo uno");
+                this.renderSliderHabitaciones(parseFloat(newRankH[0]), 1);
+            }else if(newRankH.length >= 2){
+                var menor = parseFloat(newRankH[0]);
+                var mayor = 1;
+                _.each(newRankH, function(habita){
+                    if(parseFloat(habita) >= mayor){
+                        mayor = parseFloat(habita);
+                        menor = menor;
+                    }else{
+                        mayor = menor;
+                        menor = parseFloat(habita)
+                    }
+                });
+                this.renderSliderHabitaciones(mayor, menor);
+            }else{
+                this.renderSliderHabitaciones(0, 0);
+            }
+        }
+    },
+
+    getVisibleMarkers: function(){
+        var visibleMarkers = [];
+        _.each(allPropertiesMarkers, function(marker){
+            if(marker.visible == true){
+                visibleMarkers.push(marker);
+            }
+        });
+        return(visibleMarkers);
+    },
+
+    filterByP: function(){
+
+        var valueF1 = $(".f1 option:selected").val();
+        var valueF2 = $(".f2 option:selected").val();
+        var valueF3 = $(".f3 input[type='radio']:checked").val();
+
+        var rangoPrice = $("#priceChecker").val();
+        rangoPrice = rangoPrice.split(" - ");
+        var menorP = rangoPrice[0].split("$");
+        var mayorP = rangoPrice[1].split("$");
+        var menorPrice = parseFloat(menorP[1]); 
+        var mayorPrice = parseFloat(mayorP[1]); 
+
+        if(((valueF1 == "seleccionar") || (valueF1 == "Todos")) && ((valueF2 == "seleccionar") || (valueF2 == "Todos")) && ((valueF3 == undefined) || (valueF3 == "Cualquiera"))){
+            _.each(allPropertiesMarkers, function(marker){
+                var precio = parseFloat(marker.precio);
+                if((precio >= menorPrice) && (precio <= mayorPrice)){
+                    marker.setVisible(true);
+                }else{
+                    marker.setVisible(false);
+                }
+            });
+        }else{
+            _.each(visibleMarkers, function(marker){
+                var precio = parseFloat(marker.precio);
+                if((precio >= menorPrice) && (precio <= mayorPrice)){
+                    marker.setVisible(true);
+                }else{
+                    marker.setVisible(false);
+                }
+            });
+        }
+    
+    },
+
+    filterByH: function(visibleMarkers){
+        var rangoHab = $("#habChecker").val();
+        rangoHab = rangoHab.split(" - ");
+        rangoHab[0] = parseFloat(rangoHab[0]);
+        rangoHab[1] = parseFloat(rangoHab[1]);
+        _.each(allPropertiesMarkers, function(marker){
+            var habitaciones = parseFloat(marker.habitaciones);
+            if((habitaciones >= rangoHab[0]) && (habitaciones <= rangoHab[1])){
+                marker.setVisible(true);
+            }else{
+                marker.setVisible(false);
+            }
+        });
     },
 
     filterBy1: function(e){
