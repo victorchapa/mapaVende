d98c55d11db4b0a8b508fe4a6943df23f151b799
Victor Chapa C <victor.260690@gmail.com>
2013-11-05 12:47:33 -0600

    Added Modal and its style.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <head>
     <meta charset="utf-8">
     <title>MAPA Servicios Inmobiliarios</title>
+    <link rel="stylesheet" href="assets/css/bootstrapModal.css">
     <link rel="stylesheet" href="assets/css/jQuerySliders.css">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="assets/css/normalize.css">
@@ -14,6 +15,7 @@
     <script src="assets/js/libs/backbone.js"></script>
     <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyBAxo_84zcmEXyHVMZMavZtaDs3k2NYUw4&sensor=false"></script>
     <script src="assets/js/libs/jQuerySliders.js"></script>
+    <script src="assets/js/libs/bootstrapModal.js"></script>
     <script src="assets/js/loadMap.js"></script>
   </head>
   <body>
@@ -32,6 +34,22 @@
       </div>
       <div class="backgroundFooter"></div>
     </footer>
+    <div class="modal" id="Modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h2 class="modal-title">Detalles de la Propiedad</h2>
+          </div>
+          <div id="modalDisplayer" class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-details" data-dismiss="modal">Cerrar</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
     <!-- Scripting -->
     <script src="assets/js/mapaCustomaizer.js"></script>
     <script src="assets/js/main.js"></script>
@@ -39,11 +57,13 @@
     <script src="assets/js/templates.js"></script>
     <!-- Templates -->
     <script src="templates/filters.js"></script>
+    <script src="templates/property.js"></script>
     <!-- Collections -->
     <script src="assets/js/app/collections/properties.js"></script>
     <script src="assets/js/app/collections/prices.js"></script>
     <script src="assets/js/app/collections/habitaciones.js"></script>
     <!-- Models -->
+    <script src="assets/js/app/models/property.js"></script>
     <!-- Views -->
     <script src="assets/js/app/views/indexView.js"></script>
     <script src="assets/js/app/views/filtersView.js"></script>
