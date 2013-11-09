63a2b5a1d0a7a157bc4b9b1032aff1018138701d
isracortes62 <isracortes62@gmail.com>
2013-11-09 11:41:15 -0600

    Hotfixes

--- autenticar.php
+++ autenticar.php
@@ -4,11 +4,13 @@
     <meta charset="utf-8">
 </head>
 <body>
-      <h2>Entrar al sistema</h2>
-      <form method="POST" action="api/login.php">
+	<div class='autenticar'>
+    	<h2>Entrar al sistema</h2>
+      	<form method="POST" action="api/login.php">
         <input class="span3" placeholder="Usuario" type="text" name="usuario">
-        <input class="span3" placeholder="Contraseña" type="password" name="clave">
+        <p><input class="span3" placeholder="Contraseña" type="password" name="clave"></p>
         <input type="submit" class="btn-success btn btn-Login" value="Entrar">
       </form>
+    </div>
   </body>
 <html>
